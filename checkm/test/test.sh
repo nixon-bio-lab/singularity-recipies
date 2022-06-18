8cb702c0af4ff88f44aee0bac53cbfcba601e3d4
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2022-06-18 16:36:42 +0000

    Add checkm and flye tests

---
+++ checkm/test/test.sh
@@ -0,0 +1,6 @@
+cd /opt/containers/checkm/test/
+
+# Remove any previous results
+rm -r test_results
+
+checkm test test_results
---
+++ flye/test/test.sh
@@ -0,0 +1,12 @@
+cd /opt/containers/flye/test/
+
+# Remove any previous results
+rm -r test_results
+mkdir test_results
+
+flye --version > test_results/version.txt
+
+# This command does not produce a meaningful scientfic output since the input sequence is too short.
+# However, it runs through the steps of the assembly process so makes a reasonable test in minutes 
+# instead of the hours needed for a real analysis.
+flye --nano-raw sample.fastq -o test/test_results/ --threads 16
